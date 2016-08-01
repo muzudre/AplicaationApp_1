6521cd181c2063024145e3151939d0355a70bcb9
muzudre <muzudre@gmail.com>
2016-08-01 18:38:20 +0800

    0.1.3

--- app/src/main/java/com/example/muzud/aplicaationapp_1/Contacts.java
+++ app/src/main/java/com/example/muzud/aplicaationapp_1/Contacts.java
@@ -4,14 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class Contacts extends AppCompatActivity {
+
+    WebView myBrowser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        myBrowser = (WebView)findViewById(R.id.mybrowser);
+        myBrowser.getSettings().setJavaScriptEnabled(true);
+        myBrowser.loadUrl("file:///android_asset/index.html");
 
 
     }
